b2e9c44ab76d83f89570d03fc583f6a7216fe0ed
An1419Z <angelzatarain25@gmail.com>
2024-07-18 21:40:21 -0600

    Actualizacion

--- Inicio/Registro/Registro.js
+++ Inicio/Registro/Registro.js
@@ -2,7 +2,7 @@ function handleLogin(event) {
     event.preventDefault(); // Previene la acción por defecto del formulario
     var destino;
         alert('Usuario registrado exitosamente');
-        destino = '../Inicio/inicio.html';
+        destino = '../../Index.html';
     // Redirigir al usuario al destino correspondiente
     window.location.href = destino;
 }
